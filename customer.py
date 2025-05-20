9a6a4610368d21acd62c7afb5358e2342681f223
Juma <gerald.juma@student.moringaschool.com>
2025-05-20 09:08:50 +0300

    Added name property, orders list, and related coffee tracking

--- customer.py
+++ customer.py
@@ -10,4 +10,24 @@ class Customer:
         if len(value) < 1 or len(value) > 15:
             raise ValueError("Customer name must be 1 to 15 characters long")
 
-    
+        @property
+        
+        def name(self):
+            return self.__name
+
+        @name.setter
+        def name(self, new_name):
+            self._validate_name(new_name)
+            self.__name = new_name
+
+        def orders(self):
+            return self.__orders[:]
+
+        def coffees(self):
+            seen = set()
+            unique = []
+            for order in self.__orders:
+                if order.coffee not in seen:
+                    seen.add(order.coffee)
+                    unique.append(order.coffee)
+            return unique
