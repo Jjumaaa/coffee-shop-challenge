22f590230719b2fde58ef56c4ee78160472cbed4
Juma <gerald.juma@student.moringaschool.com>
2025-05-20 08:58:43 +0300

    Defined Order class and validate constructor inputs

---
+++ order.py
@@ -0,0 +1,21 @@
+class Order:
+    def __init__(self, customer, coffee, price):
+        from customer import Customer
+        from coffee import Coffee
+
+        if type(customer) is not Customer:
+            raise TypeError("Expected a Customer instance for customer")
+        if type(coffee) is not Coffee:
+            raise TypeError("Expected a Coffee instance for coffee")
+
+        try:
+            price = float(price)
+        except ValueError:
+            raise TypeError("Price must be a float")
+
+        if price < 1.0 or price > 10.0:
+            raise ValueError("Price must be between 1.0 and 10.0")
+
+        self.__customer = customer
+        self.__coffee = coffee
+        self.__price = price
